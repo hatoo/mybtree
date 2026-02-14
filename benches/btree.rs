12e23760dd403bdce4f2af264b8e3c5097ac64ef
hatoo <hato2000@gmail.com>
2026-02-14 15:28:17 +0900

    Refactor benchmark setup by renaming and consolidating btree_insert to btree, and create new benchmarks for index operations

--- benches/btree.rs
+++ benches/btree.rs
@@ -1,12 +1,21 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use mybtree::{Btree, Pager};
 
-fn setup() -> (Btree<4096>, u64) {
+fn setup_tree() -> (Btree<4096>, u64) {
     let file = tempfile::tempfile().unwrap();
     let pager = Pager::new(file);
     let mut btree = Btree::new(pager);
     btree.init().unwrap();
     let root = btree.init_tree().unwrap();
+    (btree, root)
+}
+
+fn setup_index_tree() -> (Btree<4096>, u64) {
+    let file = tempfile::tempfile().unwrap();
+    let pager = Pager::new(file);
+    let mut btree = Btree::new(pager);
+    btree.init().unwrap();
+    let root = btree.init_index().unwrap();
     (btree, root)
 }
 
@@ -16,7 +25,7 @@ fn bench_sequential_insert(c: &mut Criterion) {
     for n in [100, 1000, 10000] {
         c.bench_function(&format!("sequential_insert_{n}"), |b| {
             b.iter(|| {
-                let (mut btree, root) = setup();
+                let (mut btree, root) = setup_tree();
                 for i in 0..n as u64 {
                     btree.insert(root, i, &value).unwrap();
                 }
@@ -24,7 +33,7 @@ fn bench_sequential_insert(c: &mut Criterion) {
             });
         });
         c.bench_function(&format!("sequential_remove_{n}"), |b| {
-            let (mut btree, root) = setup();
+            let (mut btree, root) = setup_tree();
             for i in 0..n as u64 {
                 btree.insert(root, i, &value).unwrap();
             }
@@ -37,13 +46,25 @@ fn bench_sequential_insert(c: &mut Criterion) {
         });
 
         c.bench_function(&format!("sequential_read_{n}"), |b| {
-            let (mut btree, root) = setup();
+            let (mut btree, root) = setup_tree();
             for i in 0..n as u64 {
                 btree.insert(root, i, &value).unwrap();
             }
             b.iter(|| {
                 for i in 0..n as u64 {
                     btree.read(root, i).unwrap();
+                }
+                // flush on drop
+            });
+        });
+
+        c.bench_function(&format!("sequential_index_insert_{n}"), |b| {
+            let (mut btree, root) = setup_index_tree();
+            let mut value = vec![0u8; 64];
+            b.iter(|| {
+                for i in 0..n as u64 {
+                    value[0..8].copy_from_slice(&i.to_be_bytes());
+                    btree.index_insert(root, i, value.clone()).unwrap();
                 }
                 // flush on drop
             });
