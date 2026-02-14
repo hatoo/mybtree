ed44cba2ea12cf359a5e0de0754956d3ccd05e80
hatoo <hato2000@gmail.com>
2026-02-14 18:07:12 +0900

    Refactor benchmark setup to reduce redundancy in sequential insert tests

--- benches/btree.rs
+++ benches/btree.rs
@@ -26,8 +26,8 @@ fn bench_sequential_insert(c: &mut Criterion) {
 
     for n in [100, 1000, 10000] {
         c.bench_function(&format!("sequential_insert_{n}"), |b| {
+            let (mut btree, root) = setup_tree();
             b.iter(|| {
-                let (mut btree, root) = setup_tree();
                 for i in 0..n as u64 {
                     btree.insert(root, i, &value).unwrap();
                 }
